0b44ab7f03733ca0cb34ed8e0ef5430350d73bd5
ayushianand10 <ayushianand9113@gmail.com>
2026-02-20 11:08:45 +0530

    20-02-26

--- Subarrays.java
+++ Subarrays.java
@@ -2,16 +2,24 @@ public class Subarrays {
     public static void maxSubarraySum(int numbers[]){
       int currSum=0;
       int maxSum=Integer.MIN_VALUE;
+      int prefix[]=new int[numbers.length];
+
+      prefix[0]=numbers[0];
+      //calculate prefix array
+      for(int i=1;i<prefix.length;i++){
+         prefix[i]=prefix[i-1]+numbers[i];
+      }
       
         for(int i=0;i<numbers.length;i++){
             int start =i;
             for(int j=i;j<numbers.length;j++){
                 int end=j;
-                currSum=0;
-                for(int k=start;k<=end;k++){//print
+                currSum= start == 0 ? prefix[end] : prefix[end]-prefix[start-1];
+                //currSum=0;
+                //for(int k=start;k<=end;k++){//print
                    /*  System.out.print(numbers[k]+" "); print subarray */
-                   currSum += numbers[k];
-                }
+                  // currSum += numbers[k];
+                //}
                 System.out.println(currSum);
                 if(maxSum < currSum) {
                     maxSum = currSum;
@@ -23,9 +31,24 @@ public class Subarrays {
         }
         System.out.println("max sum = "+maxSum);
     }
+
+    public static void kadanes(int numbers[]){
+       int ms = Integer.MIN_VALUE;
+       int cs = 0;
+
+       for(int i=0;i<numbers.length;i++){
+        cs = cs+numbers[i];
+        if(cs<0)
+        {cs=0;}
+        ms = Math.max(cs,ms);
+       }
+       System.out.println("our max subarray sum is : "+ms);
+    }
     public static void main(String args[]){
-        int numbers[]={2,4,6,8,10};
-       maxSubarraySum(numbers);
+        int numbers[]={-2,-3,4,-1,-2,1,5,-3};
+        //int numbers[]={2,4,6,8,10};
+       //maxSubarraySum(numbers);
+       kadanes(numbers);
     }
     
 }
